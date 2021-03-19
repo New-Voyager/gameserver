9eb7bc899f08e57711916e8b0dbcdb9e5275c463
Yong Cho <yongjec@gmail.com>
2021-03-18 22:17:29 -0400

    Use docker-compose for system test

--- system_test/docker-test.sh
+++ system_test/docker-test.sh
@@ -1,55 +1,9 @@
 #!/usr/bin/env bash
 
-# This script is used to launch the system test in the docker container.
+# export API_SERVER_URL='http://localhost:9501'
 
-set -e
+export BOT_SCRIPT='botrunner_scripts/river-action-3-bots.yaml'
 
-check_api_server() {
-    http_status=$(curl -s --max-time 3 --show-error -o /dev/null -w "%{http_code}" ${API_SERVER_URL})
-}
-
-errs=0
-
-if [ -z ${API_SERVER_URL} ]; then
-    echo "API_SERVER_URL is not defined."
-    ((errs++))
-fi
-
-if [ -z ${PERSIST_METHOD} ]; then
-    echo "PERSIST_METHOD is not defined."
-    ((errs++))
-fi
-
-if [ -z ${REDIS_HOST} ]; then
-    echo "REDIS_HOST is not defined."
-    ((errs++))
-fi
-
-if [ -z ${REDIS_PORT} ]; then
-    echo "REDIS_PORT is not defined."
-    ((errs++))
-fi
-
-if [ -z ${REDIS_DB} ]; then
-    echo "REDIS_DB is not defined."
-    ((errs++))
-fi
-
-if [ ${errs} -ne 0 ]; then
-    exit 1
-fi
-
-# Wait for the api server to be ready.
-MAX_WAIT=${MAX_WAIT:-30}
-remaining=${MAX_WAIT}
-until curl -m 2 ${API_SERVER_URL}; do
-    ((remaining--))
-    if [ ${remaining} -le 0 ]; then
-        echo "API server not responded in ${MAX_WAIT} attempts"
-        exit 1
-    fi
-    echo "Waiting for api server (${remaining})"
-    sleep 1
-done;
-
-./test --config config.yaml --game-server-dir=../server --botrunner-dir=../botrunner
+docker exec -t system_test_botrunner_1 \
+    ./botrunner \
+    --script ${BOT_SCRIPT}
