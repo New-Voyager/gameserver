f88dd172a8ae68ab7881dd3fadd704f610c8e3b1
Yong Cho <yongjec@gmail.com>
2021-04-25 14:51:45 -0400

    Reorganize system test scripts

--- botrunner/system_test.sh
+++ botrunner/system_test.sh
@@ -4,23 +4,25 @@ set -e
 
 # YAML file paths are relative to the botrunner.
 test_scripts=(
-    'botrunner_scripts/river-action-3-bots.yaml'
-    'botrunner_scripts/crash_test/river-action-3-bots-wait-for-next-action.yaml'
-    'botrunner_scripts/crash_test/river-action-3-bots-move-to-next-action.yaml'
-    'botrunner_scripts/crash_test/river-action-3-bots-move-to-next-round.yaml'
+    'botrunner_scripts/system_test/river-action-3-bots.yaml'
+    'botrunner_scripts/system_test/river-action-3-bots-wait-for-next-action.yaml'
+    'botrunner_scripts/system_test/river-action-3-bots-move-to-next-action.yaml'
+    'botrunner_scripts/system_test/river-action-3-bots-move-to-next-round.yaml'
 )
 
-generated_dir='botrunner_scripts/crash_test/generated/'
+# Generate additional scripts based on templates.
+generated_dir='botrunner_scripts/system_test/generated/'
 mkdir -p "${generated_dir}"
 for seq in {1..8}; do
     out_file="${generated_dir}/3-bots-all-players-all-in-${seq}.yaml"
-    sed "s/{{SEQ}}/${seq}/g" 'botrunner_scripts/crash_test/template/3-bots-all-players-all-in-template.yaml' > ${out_file} && \
+    sed "s/{{SEQ}}/${seq}/g" 'botrunner_scripts/system_test/template/3-bots-all-players-all-in-template.yaml' > ${out_file} && \
     test_scripts+=(${out_file})
 done
 
 echo "Test Scripts: ${test_scripts[@]}"
 
 for script in "${test_scripts[@]}"; do
+    exit_code=0
     docker exec -t system_test_botrunner_1 bash -c "\
         while ! curl -s \${API_SERVER_URL} >/dev/null; do \
             echo Waiting for API server \${API_SERVER_URL}; \
@@ -33,5 +35,11 @@ for script in "${test_scripts[@]}"; do
         done \
         && \
         ./botrunner --script ${script} \
-    "
+    " || exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "System test failed on script ${script}"
+        exit $exit_code
+    fi
 done
+
+echo "Finished system test. Tested Scripts: ${test_scripts[@]}"
