b87738a18de5cbde13ba770a466a44781ff79520
Yong Cho <yongjec@gmail.com>
2021-05-29 14:34:55 -0400

    Timer calls restart-timer on startup

--- timer/main.go
+++ timer/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/rs/zerolog/log"
+	"voyager.com/timer/internal/util"
 	"voyager.com/timer/rest"
 )
 
@@ -22,5 +27,46 @@ func init() {
 }
 func main() {
 	mainLogger.Info().Msgf("Port: %d", cmdArgs.port)
-	rest.RunServer(cmdArgs.port)
+	go rest.RunServer(cmdArgs.port)
+
+	time.Sleep(1 * time.Second)
+	apiServerURL := util.Env.GetAPIServerURL()
+	mainLogger.Info().Msg("Requesting to restart the active games.")
+	waitForAPIServer(apiServerURL)
+	requestRestartTimers(apiServerURL)
+
+	select {}
+}
+
+func waitForAPIServer(apiServerURL string) {
+	readyURL := fmt.Sprintf("%s/internal/ready", apiServerURL)
+	client := http.Client{Timeout: 2 * time.Second}
+	for {
+		mainLogger.Info().Msgf("Checking API server ready")
+		resp, err := client.Get(readyURL)
+		if err == nil && resp.StatusCode == 200 {
+			resp.Body.Close()
+			break
+		}
+		if err == nil {
+			mainLogger.Error().Msgf("%s returend %d", readyURL, resp.StatusCode)
+			resp.Body.Close()
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func requestRestartTimers(apiServerURL string) {
+	var err error
+
+	restartURL := fmt.Sprintf("%s/internal/restart-timers", apiServerURL)
+	resp, err := http.Post(restartURL, "application/json", bytes.NewBuffer([]byte{}))
+	if err != nil {
+		mainLogger.Fatal().Msg(fmt.Sprintf("Failed to restart timers. Error: %s", err.Error()))
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		mainLogger.Fatal().Msg(fmt.Sprintf("Failed to restart timers. Error: %d", resp.StatusCode))
+	}
 }
