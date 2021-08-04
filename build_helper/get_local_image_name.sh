e25b2e29de355371ed916d8b0b6497f01b446a24
Yong Cho <yongjec@gmail.com>
2021-08-03 22:05:02 -0400

    Use common scheme to distinguish image name between builds

--- build_helper/get_local_image_name.sh
+++ build_helper/get_local_image_name.sh
@@ -1,7 +1,8 @@
 set -e
 
-if [ "$#" -eq 2 ]; then
-    echo $1:$2
+if ! [ -z ${CI} ]; then
+    branch_suffix=$(echo ${GIT_BRANCH} | sed 's/\//_/g' | sed 's/-/_/g')
+    echo $1:${branch_suffix}_${BUILD_ID}
 else
     echo $1
 fi
--- common.mk
+++
@@ -1,3 +0,0 @@
-ifdef CI
-DOCKER_IMAGE_TAG := $(subst $(_slash),$(_underscore),$(GIT_BRANCH))__$(BUILD_ID)
-endif
