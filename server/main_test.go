1d33aecc5291936c98dd04d31f041dc8e18ae60e
Yong Cho <yongjec@gmail.com>
2021-08-11 23:06:41 -0400

    minor comment

--- server/main_test.go
+++ server/main_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 // TestRunMain is used to run the server within the test binary so
-// that we can get the coverage while running the app normally.
+// that we can get the coverage while running the main function normally.
 // The command line arguments for the app should be passed in to the
 // test binary before the test arguments.
 //
 // For example,
 //
 // go test -coverpkg=./... -c -o game-server.test
-// ./server.test --script-tests -test.coverprofile=coverage.out -test.run TestRunMain voyager.com/server
+// ./game-server.test --script-tests -test.coverprofile=coverage.out -test.run TestRunMain voyager.com/server
 //
 // Here is a good resource on how to build and use the test binary.
 // https://www.elastic.co/blog/code-coverage-for-your-golang-system-tests
