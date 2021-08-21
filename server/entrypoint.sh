d9642fa29b0db7cf12a6502b4633ab714b528994
Yong Cho <yongjec@gmail.com>
2021-08-21 13:00:22 -0400

    Remove build mode; Build both binaries in one image

---
+++ server/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+RESTART_DELAY_SEC=2
+
+while true; do
+    /app/game-server --server
+    echo "Game server exited with $?. Restarting in ${RESTART_DELAY_SEC} seconds."
+    sleep ${RESTART_DELAY_SEC}
+done
