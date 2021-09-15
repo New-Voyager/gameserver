e9d0d5d42ea9c0a2bcf4e043d26383d1c2e4e825
Yong Cho <yongjec@gmail.com>
2021-09-14 22:40:58 -0400

    Don't verify api in auto-play

--- botrunner/internal/player/bot_player.go
+++ botrunner/internal/player/bot_player.go
@@ -1392,6 +1392,10 @@ func (bp *BotPlayer) verifyPotWinners(actualPot *game.PotWinners, expectedPot ga
 }
 
 func (bp *BotPlayer) verifyAPIRespForHand() {
+	if bp.config.Script.AutoPlay {
+		return
+	}
+
 	bp.logger.Info().Msgf("%s: Verifying api responses", bp.logPrefix)
 
 	passed := bp.VerifyAPIResponses(bp.gameCode, bp.config.Script.GetHand(bp.game.handNum).APIVerification)
