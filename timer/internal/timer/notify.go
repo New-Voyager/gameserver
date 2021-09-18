a956eec956463b26145116df74531ec4eded2efb
Yong Cho <yongjec@gmail.com>
2021-09-18 11:09:39 -0400

    Don't crash on timer notify error

--- timer/internal/timer/notify.go
+++ timer/internal/timer/notify.go
@@ -8,28 +8,32 @@ import (
 	"voyager.com/timer/internal/util"
 )
 
-var APIServerURL string
-
-func init() {
-	APIServerURL = util.Env.GetAPIServerURL()
-}
+var (
+	APIServerURL     = util.Env.GetAPIServerURL()
+	maxRetries       = 3
+	retryDelayMillis = 2000
+)
 
 func notifyTimeout(t *Timer) {
 	timerLogger.Info().Msgf("Notifying timeout %d|%d|%s", t.gameID, t.playerID, t.purpose)
 	url := fmt.Sprintf("%s/internal/timer-callback/gameId/%d/playerId/%d/purpose/%s", APIServerURL, t.gameID, t.playerID, t.purpose)
-	retry := true
-	for retry {
-		resp, _ := http.Post(url, "application/json", nil)
-		if resp == nil {
-			// sleep for 5 seconds and retry
-			time.Sleep(5 * time.Second)
-			continue
-		}
-		defer resp.Body.Close()
 
-		if resp.StatusCode != 200 {
-			timerLogger.Fatal().Uint64("game", t.gameID).Msg(fmt.Sprintf("Failed to call timer callback. Error: %d", resp.StatusCode))
-		}
-		retry = false
+	retries := 0
+	resp, err := http.Post(url, "application/json", nil)
+	for err != nil && retries < int(maxRetries) {
+		retries++
+		timerLogger.Error().Msgf("Error in post %s: %s. Retrying (%d/%d)", url, err, retries, maxRetries)
+		time.Sleep(time.Duration(retryDelayMillis) * time.Millisecond)
+		resp, err = http.Post(url, "application/json", nil)
+	}
+
+	if err != nil {
+		timerLogger.Error().Uint64("game", t.gameID).Msgf("Retry exhausted for post %s", url)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		timerLogger.Error().Uint64("game", t.gameID).Msgf("Received http status %d from %s", resp.StatusCode, url)
 	}
 }
