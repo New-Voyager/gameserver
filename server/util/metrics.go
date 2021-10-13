5d4fcafc8f741dcf7293387696b49d7fe54d1aeb
Yong Cho <yongjec@gmail.com>
2021-10-13 17:28:37 -0400

    Add some experimental metrics new hand / ended hand

--- server/util/metrics.go
+++ server/util/metrics.go
@@ -7,11 +7,21 @@ import (
 
 type metrics struct {
 	newGameReceivedCounter prometheus.Counter
+	newHandMsgSentCounter  prometheus.Counter
+	handEndMsgSentCounter  prometheus.Counter
 	activeGamesGauge       prometheus.Gauge
 }
 
 func (m *metrics) NewGameReceived() {
 	m.newGameReceivedCounter.Inc()
+}
+
+func (m *metrics) NewHandMsgSent() {
+	m.newHandMsgSentCounter.Inc()
+}
+
+func (m *metrics) HandEndMsgSent() {
+	m.handEndMsgSentCounter.Inc()
 }
 
 func (m *metrics) SetNumActiveGames(numActiveGames int) {
@@ -22,6 +32,14 @@ var Metrics = &metrics{
 	newGameReceivedCounter: promauto.NewCounter(prometheus.CounterOpts{
 		Name: "new_games_received_total",
 		Help: "Total number of new games received",
+	}),
+	newHandMsgSentCounter: promauto.NewCounter(prometheus.CounterOpts{
+		Name: "new_hand_msg_sent_total",
+		Help: "Total number of NEW_HAND messages sent",
+	}),
+	handEndMsgSentCounter: promauto.NewCounter(prometheus.CounterOpts{
+		Name: "hand_end_msg_sent_total",
+		Help: "Total number of hand END messages sent",
 	}),
 	activeGamesGauge: promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "num_active_games",
